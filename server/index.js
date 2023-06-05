337dc2c8fdedade071ca739282ae0544eee98c63
Vinicius Teodoro <vinitbasilio@hotmail.com>
2023-06-05 08:54:53 -0300

    Teste usando server na ec2 pt2

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const db = mysql.createPool({
-  host: proccess.env.MYSQL_HOST,
-  port: proccess.env.MYSQL_PORT,
-  user: proccess.env.MYSQL_USER,
-  password: proccess.env.MYSQL_PASSWORD,
-  database: proccess.env.MYSQL_DATABASE
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 })
 
 app.use(cors())
